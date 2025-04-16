3764c175038fc1a64d997efbce2a9ceed9f9462e
Reese Trcalek <rtgca613@yahoo.com>
2025-04-16 09:20:18 -0500

    Add files via upload
    Early stages of the program

---
+++ accounts.h
@@ -0,0 +1,12 @@
+#include <string>
+using namespace std;
+
+struct Node {
+    string user = "";
+    int Type = 0;
+    Node* next = nullptr;
+};
+
+class Accounts {
+
+};
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "accounts.h"
+using namespace std;
+
+int main() {
+
+}
